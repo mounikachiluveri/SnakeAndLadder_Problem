b7119382c3b2bf0cc4be9422475a87645a8eadf2
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-15 19:12:01 +0530

    [Mounika] Added:Random function used to get the dice result between 1 to 6

--- SnakeandLadder.sh
+++ SnakeandLadder.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 echo "=============Welcome to Snake and ladder program=============="
-positionOfPlayer=0
+function diceRoll(){
+    dieResult=$((RANDOM % 6 + 1))
+    positionOfPlayer=$(($positionOfPlayer+$dieResult))
+    echo "positionOfPlayer:$positionOfPlayer"
+}
+diceRoll
