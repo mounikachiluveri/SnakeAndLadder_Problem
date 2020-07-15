0c62e394e0d69d86be1fe5e99324179ab4036060
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-15 20:13:45 +0530

    [Mounika] Added:Repeat the Loop until winning position of the player

--- SnakeandLadder.sh
+++ SnakeandLadder.sh
@@ -1,32 +1,76 @@
 #!/bin/bash -x
 echo "=============Welcome to Snake and ladder program=============="
-positionOfPlayer=0
 NO_PLAY=0
 LADDER=1
 SNAKE=2
+positionOfPlayer=0
+chance=1
+WINNING_POSITION=100
+dieResult=0
+checkOptions=0
+STARTINGPOSITION=0
+numberOfTimesDiceroll=0
+playerOne=0
+playerTwo=0
+declare -a playerOne
+declare -a playerTwo
+
 function noPlay(){
    positionOfPlayer=$((positionOfPlayer+0))
+   return $positionOfPlayer
 }
 
 function ladder(){
-   positionOfPlayer=$((positionOfPlayer+$dieResult))
+   if [ $((positionOfPlayer+$dieResult)) -gt $WINNING_POSITION ]
+   then
+        positionOfPlayer=$((positionOfPlayer-$dieResult))
+   else
+        positionOfPlayer=$((positionOfPlayer+$dieResult))
+   fi
+   return $positionOfPlayer
+   #diceRoll $playerOne
 }
 
 function snake(){
-  positionOfPlayer=$((positionOfPlayer-$dieResult))
+   if [ $((positionOfPlayer-$dieResult)) -lt $STARTINGPOSITION ]
+   then
+       positionOfPlayer=$STARTINGPOSITION
+   else
+        positionOfPlayer=$((positionOfPlayer-$dieResult))
+   fi
+   return $positionOfPlayer
 }
 
 function diceRoll(){
-   dieResult=$((RANDOM % 6 + 1))
-   checkOptions=$((RANDOM % 3))
+ positionOfPlayer=$1
+ dieResult=$((RANDOM % 6 + 1))
+ checkOptions=$((RANDOM % 3))
    case $checkOptions in
         $NO_PLAY)
-             noPlay;;
+               noPlay;;
         $LADDER)
-             ladder;;
+               ladder;;
         $SNAKE)
-             snake;;
-   esac
+               snake;;
+        esac
 }
-diceRoll
-echo $positionOfPlayer
+
+function switchToPlayer(){
+   while [ $positionOfPlayer -le $WINNING_POSITION ]
+   do
+      ((numberOfTimesDiceroll++))
+       if [ $chance -eq 1 ]
+       then
+         diceRoll $playerOne
+         playerOne=$?
+         playerOne[$numberOfTimesDiceRoll]=$playerOne
+         echo "Player1 position :" $playerOne
+         if [ $playerOne -eq $WINNING_POSITION ]
+         then
+            echo "player1 win"
+         break
+         fi
+      fi
+  done
+}
+switchToPlayer
